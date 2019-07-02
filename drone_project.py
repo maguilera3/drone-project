32c0387dc05754079c8fb05e29d8ca128a48d1b1
Kinozuko <victorcmendozaa@gmail.com>
2019-07-02 19:44:46 -0400

    Command threading

---
+++ drone_project.py
@@ -0,0 +1,50 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+import socket
+import threading 
+import time
+
+app = Flask(__name__)
+api = Api(app)
+
+host = ''
+port = 9000
+locaddr = (host,port) 
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+tello_ip = '192.168.10.1'
+tello_address = (tello_ip, 8889)
+sock.bind(locaddr)
+
+video_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+video_socket.bind(('0.0.0.0', 11111))
+
+def command(command='command'):
+    global sock
+    if command=='command':
+        while True:
+            sock.sendto(command.encode(encoding="utf-8") , tello_address)
+            time.sleep(5)
+    else:
+        sock.sendto(command.encode(encoding="utf-8") , tello_address)
+
+def video(command):
+    global video_socket
+    while True:
+        msg, ip = video_socket.recvfrom(2048)
+        server_socket.sendto(msg,('192.168.1.101',9999))
+class Control(Resource):
+    def post(self):
+        command_data = request.form['command']
+        if not command_data=='land':
+            threading.Thread(target=command,args=(command_data,)).start()
+            threading.Thread(target=command).start()
+        if command_data=='streamon':
+           threading.Thread(target=command,args=(command_data,)).start()
+        else:
+            threading.Thread(target=command,args=('land',)).start()
+    
+api.add_resource(Control, '/control')
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0')
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from drone_project import app
+
+if __name__ == '__main__':
+	app.run()
